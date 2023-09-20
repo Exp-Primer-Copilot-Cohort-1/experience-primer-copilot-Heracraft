4e639469b91937db54f8b3e4e758ad487fa47370
Nehemia <66851037+Heracraft@users.noreply.github.com>
2023-09-20 21:04:23 +0000

    server

---
+++ comments.js
@@ -0,0 +1,18 @@
+// create a web server
+// 1. npm init -y
+// 2. npm install express
+// 3. node comments.js
+// 4. open browser and go to http://localhost:3000/comments
+
+const express = require('express'); // import express
+const app = express(); // create an instance of express
+
+// create a route
+app.get('/comments', (req, res) => {
+    res.send('comments');
+});
+
+// start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
